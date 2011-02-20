b3e527a0ab9200a8f76ca9b5efeefdb8865edfa2
Mike Bloy <mike@bloy.org>
2011-02-20 09:35:05 -0600

    further support of session-per-task tmuxing

--- bin/tmux-default.sh
+++ bin/tmux-default.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-tmux has-session -t default && tmux -2 attach-session -t default || tmux -2 new-session -s default
+tmux has-session -t default
+if [ $? -eq 1 ]; then
+        tmux new-session -s default -d
+        if [ -e $HOME/bin/tmux-default-new-session ]; then
+                . $HOME/bin/tmux-default-new-session
+        fi
+fi
+exec tmux -2 attach-session -t default
