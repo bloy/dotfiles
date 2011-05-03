aed14f89854addca96fd9041ecc7ca98d744365c
Mike Bloy <mike@bloy.org>
2011-05-03 09:38:53 -0500

    add -u flag to tmux scripts

--- bin/tmux-default.sh
+++ bin/tmux-default.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 tmux has-session -t default
 if [ $? -eq 1 ]; then
-        tmux new-session -s default -d
+        tmux -u new-session -s default -d
         if [ -e $HOME/bin/tmux-default-new-session ]; then
                 . $HOME/bin/tmux-default-new-session
         fi
 fi
-exec tmux -2 attach-session -t default
+exec tmux -u -2 attach-session -t default
