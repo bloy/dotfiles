56eee892a12caa9e6e5af76e84ed4b174fb8b98d
Mike Bloy <mike@bloy.org>
2014-09-20 16:52:57 -0500

    swwt tmuxifier session

---
+++ tmux/swwt.session.sh
@@ -0,0 +1,17 @@
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+session_root "~/projects/savage-worlds-web-tools"
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "swwt"; then
+  # Load a defined window layout.
+  load_window "vim_and_shell"
+
+  # Select the default active window on session creation.
+  select_window 1
+
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
