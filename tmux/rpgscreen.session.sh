964084cfd2d2805ef4073fe38c73a2eb55bc234e
Mike Bloy <mike@bloy.org>
2015-03-10 18:16:18 -0500

    better rpgscreen tmux session

--- tmux/rpgscreen.session.sh
+++ tmux/rpgscreen.session.sh
@@ -1,11 +1,17 @@
 session_root "~/projects/rpgscreen"
 
-VIRTUAL_ENV=`pew in rpgscreen env | grep VIRTUAL_ENV | cut -d= -f 2`
+VIRTUAL_ENV="$HOME/projects/rpgscreen/.virtualenv"
 
 if initialize_session "rpgscreen"; then
 
   tmuxifier-tmux set-environment VIRTUAL_ENV $VIRTUAL_ENV
   load_window "vim_and_shell"
+
+  new_window "server"
+  run_cmd 'grunt'
+  split_v 50
+  run_cmd './manage.py runserver'
+
   select_window 1
 
 fi
