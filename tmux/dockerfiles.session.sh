f8856a99f4d7ff67f97f9ff5c80850f63f623409
Mike Bloy <mike@bloy.org>
2016-12-31 12:51:53 -0600

    move tmux related files to tmux directory

--- tmux/dockerfiles.session.sh
+++
@@ -1,8 +0,0 @@
-session_root "~/projects/dockerfiles"
-
-if initialize_session "dockerfiles"; then
-  load_window "vim_and_shell"
-fi
-
-# Finalize session creation and switch/attach to it.
-finalize_and_go_to_session
